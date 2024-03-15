2ffb28c583d899c166da6cca0ab42fa5a7416551
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-03-16 00:36:57 +0530

    Updated Solution - GfG to GitHub

--- Easy/Delete without head pointer/delete-without-head-pointer.cpp
+++ Easy/Delete without head pointer/delete-without-head-pointer.cpp
@@ -83,7 +83,7 @@ class Solution
     {
        // Your code here
        del_node->data = del_node->next->data;
-         del_node->next = del_node->next->next;
+        del_node->next = del_node->next->next;
     }
 
 };

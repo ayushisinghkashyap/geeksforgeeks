e4cc1a5dce26bdfdcfbdbfa9009379254f964716
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-03-23 11:03:48 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Fibonacci series up to Nth term/fibonacci-series-up-to-nth-term.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            int ans[] = obj.Series(n);
+            for (int i : ans) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    int[] Series(int n) {
+        // code here
+        int a[]= new int[n+1];
+        a[1]=1;
+        for(int i=2;i<=n;i++){
+            a[i]=(a[i-1]+a[i-2])%1000_000_007;
+        }
+        return a;
+        
+    }
+}

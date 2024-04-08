e27ba362024c45cc2b03debc2d60df843b4cfc59
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-04-09 02:28:40 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Minimum Points To Reach Destination/minimum-points-to-reach-destination.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            int points[][] = new int[m][n];
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++) points[i][j] = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.minPoints(points, m, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int minPoints(int points[][], int m, int n) {
+        // Your code goes here
+         int[][] dp = new int[m][n];
+        dp[m - 1][n - 1] = Math.max(1, 1 - points[m - 1][n - 1]);
+        for (int i = m - 2; i >= 0; i--) {
+            dp[i][n - 1] = Math.max(1, dp[i + 1][n - 1] - points[i][n - 1]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            dp[m - 1][i] = Math.max(1, dp[m - 1][i + 1] - points[m - 1][i]);
+        }
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = Math.max(1, Math.min(dp[i + 1][j], dp[i][j + 1]) - points[i][j]);
+            }
+        }
+        return dp[0][0];
+    }
+}

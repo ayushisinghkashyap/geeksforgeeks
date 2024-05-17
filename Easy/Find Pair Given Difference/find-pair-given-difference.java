3330045631e5e38233ac817b0859cb61474c20a7
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-05-17 16:18:35 +0530

    Updated Solution - GfG to GitHub

--- Easy/Find Pair Given Difference/find-pair-given-difference.java
+++ Easy/Find Pair Given Difference/find-pair-given-difference.java
@@ -53,15 +53,15 @@ class Solution {
     public int findPair(int n, int x, int[] arr) {
         // code here
         Arrays.sort(arr);
-        int i=0, j=1;
-        while(i<n && j<n){
+int i=0, j=1;
+while(i<n && j<n){
     
-            if(i!=j && arr[j]-arr[i]==x)return 1;
-            else if(arr[j]-arr[i]<x)j++;
-            else
-            i++;
+    if(i!=j && arr[j]-arr[i]==x)return 1;
+     else if(arr[j]-arr[i]<x)j++;
+     else
+     i++;
 
-        }
-        return -1;
+}
+return -1;
     }
 }

2ce7a0ae7a747150047dcc95bd2cb031eaf74b6c
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-05-19 21:27:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find the closest number/find-the-closest-number.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int k;
+            k = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.findClosest(n, k, arr);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int findClosest(int n, int k, int[] arr) {
+        // code here
+        for(int i=0;i<arr.length;i++){
+            if(k<=arr[i]){
+                if(i==0)return arr[i];
+                else{
+                    if(k==arr[i])return arr[i];
+                    else{
+                        int x=Math.abs(k-arr[i]);
+                        int y=Math.abs(k-arr[i-1]);
+                        if(x>y)return arr[i-1];
+                        else if(x<=y)return arr[i];
+                    }
+                }
+            }
+            
+        }
+        return arr[n-1];
+    }
+}
+        

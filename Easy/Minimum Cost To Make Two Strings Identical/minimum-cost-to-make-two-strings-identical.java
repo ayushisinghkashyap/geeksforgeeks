8586f250e0322e710674202ff4bb0a08d07ba81a
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-05-26 22:35:17 +0530

    Updated Solution - GfG to GitHub

--- Easy/Minimum Cost To Make Two Strings Identical/minimum-cost-to-make-two-strings-identical.java
+++ Easy/Minimum Cost To Make Two Strings Identical/minimum-cost-to-make-two-strings-identical.java
@@ -24,7 +24,7 @@ class GfG {
 
 class Solution {
     public int findMinCost(String x, String y, int costX, int costY) {
-    int [][] dp = new int[x.length()+1][y.length()+1];
+        int [][] dp = new int[x.length()+1][y.length()+1];
         for(int i=dp.length-1; i>=0; i--){
             for(int j=dp[0].length-1; j>=0; j--){
                 if(i == dp.length-1 && j == dp[0].length-1){
@@ -49,5 +49,5 @@ class Solution {
         int y1 = y.length()-lcs;
         int cost = x1*costX + y1*costY;
         return cost;
-}
+    }
 }

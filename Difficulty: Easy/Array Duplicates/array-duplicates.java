1384a38b67209497ce9ccd794a9066470536f0b3
Ayushi Singh <ayushisinghpatel373@gmail.com>
2025-04-24 23:18:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            List<Integer> result = new Solution().findDuplicates(arr);
+            Collections.sort(result);
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();
+            }
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public ArrayList<Integer> findDuplicates(int[] arr) {
+        ArrayList<Integer> num = new ArrayList<>();
+        Arrays.sort(arr);
+        for(int i = 1;i<arr.length;i++){
+            if(arr[i] == arr[i-1]){
+                num.add(arr[i-1]);
+                
+            }
+        }
+        return num;
+    }
+}
